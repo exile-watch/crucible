6c7718ce4732ff71d2d1caa062c915eba5ea90ca
sbsrnt <eskrzy@gmail.com>
2021-03-06 21:16:07 +0100

    feat: change map header to h2 and add active color

--- components/Map/MapBossesHeading.tsx
+++ components/Map/MapBossesHeading.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import cn from "classnames";
-import styles from "#components/Map/Map.module.scss";
+import styles from "./Map.module.scss";
 import Heading from "#components/Heading/Heading";
 import Link from "next/link";
 import {kebabCase} from "lodash";
 import {useRouter} from "next/router";
+import cx from "classnames";
 
 const MapBossesHeading = ({data}) => {
-  const { query: { category, map } } = useRouter()
+  const { query: { category, map, boss: queryBoss } } = useRouter()
 
   return (
     <div className={cn('', styles.bosses)}>
       {data?.bosses.map(boss => {
         const [bossName] = Object.keys(boss);
+        const isActive = kebabCase(bossName) === queryBoss;
 
         return (
-          <Heading as="h1">
-            <Link href={`/${category}/${map}/${kebabCase(bossName)}`}>{bossName}</Link>
+          <Heading as="h2">
+            <Link href={`/${category}/${map}/${kebabCase(bossName)}`}>
+              <a className={cx(isActive ? styles.active : undefined, styles.link)}>{bossName}</a>
+            </Link>
           </Heading>
         )
       })}
