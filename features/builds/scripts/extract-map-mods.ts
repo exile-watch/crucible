9402bc243b9e6988ac561386a45f75b22ae26447
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-17 16:43:57 +0200

    feat(builds): add map mods data

---
+++ features/builds/scripts/extract-map-mods.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+import colorifyConsole from '../../../utils/colorifyConsole';
+
+const rootPath = './features/builds';
+const tokensPath = `${rootPath}/tokens`;
+const extractedDataPath = `${rootPath}/extracted-data`;
+
+const getExtractedData = async () => {
+  // @ts-ignore
+  await console.time(colorifyConsole({ label: 'time', text: 'Extract Map Mods' }));
+  return yaml.load(fs.readFileSync(`${tokensPath}/map-mods.yml`, 'utf8'));
+};
+
+getExtractedData()
+  .then(async (extractedData) => {
+    await fs.writeFileSync(`${extractedDataPath}/map-mods.json`, JSON.stringify(extractedData));
+    // @ts-ignore
+    await console.timeEnd(colorifyConsole({ label: 'time', text: 'Extract Map Mods' }));
+  })
+  .catch(async (err) => {
+    await console.log(err);
+    // @ts-ignore
+    await console.timeEnd(colorifyConsole({ label: 'time', text: 'Extract Map Mods' }));
+  });
