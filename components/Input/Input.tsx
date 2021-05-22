818358f2abe593a2a382e8dca6e1a1a3b3fd9824
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-23 01:46:33 +0200

    feat(components): add size prop to input and improve types

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,11 +1,38 @@
 import React, { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import cx from 'classnames';
 
 import styles from './Input.module.scss';
 
-type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+type InputProps = {
+  label?: string;
+  size?: 'medium' | 'large';
+  value: string;
+} & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'size'>;
 
-const Input = ({ onChange, value, id, ...props }: InputProps) => {
-  return <input id={id} className={styles.input} onChange={onChange} value={value} {...props} />;
+const Input = ({
+  id,
+  value,
+  placeholder,
+  label,
+  className,
+  size = 'medium',
+  ...props
+}: InputProps) => {
+  const isActive = value?.length > 0;
+
+  return (
+    <div className={cx('theme-transition-scope', styles.inputWrapper)}>
+      <input
+        value={value}
+        placeholder={placeholder}
+        className={cx(styles.input, styles[size], className)}
+        {...props}
+      />
+      <label htmlFor={id} className={cx(styles.label, isActive && styles.isActive)}>
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default Input;
