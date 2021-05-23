fa70f5570141db9b486147fa72c16eb139732156
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-24 01:05:26 +0200

    feat(storybook): add heading stories

--- client/design-system/components/Heading/Heading.tsx
+++ client/design-system/components/Heading/Heading.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames';
 
 import styles from './Heading.module.scss';
 
-type HeadingProps = {
+export type HeadingProps = {
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
   children: ReactNode;
   className?: string;
