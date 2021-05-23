346def2b3c346ae33a16a566168296639cbfe6c1
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-24 01:05:15 +0200

    feat(storybook): add card stories

--- client/design-system/components/Card/Card.stories.tsx
+++ client/design-system/components/Card/Card.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meta, Story } from '@storybook/react';
 
 import { TextEditor } from '../../../design-system/components';
@@ -12,6 +12,15 @@ export default {
       defaultValue: false,
       control: 'boolean',
     },
+    className: {
+      control: 'disabled',
+    },
+    style: {
+      control: 'disabled',
+    },
+    as: {
+      control: 'disabled',
+    },
   },
 } as Meta;
 
@@ -21,16 +30,16 @@ export const WithParagraph: Story<CardProps> = (args) => (
   </Card>
 );
 
-export const WithTextEditor: Story<CardProps> = (args) => (
-  <Card {...args}>
-    <TextEditor
-      value={[
-        {
-          type: 'paragraph',
-          children: [{ text: 'Some text editor value' }],
-        },
-      ]}
-      onChange={() => ({})}
-    />
-  </Card>
-);
+export const WithTextEditor: Story<CardProps> = (args) => {
+  const [value, setValue]: any = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'Some text editor value' }],
+    },
+  ]);
+  return (
+    <Card {...args}>
+      <TextEditor value={value} onChange={setValue} />
+    </Card>
+  );
+};
