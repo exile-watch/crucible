551ac93c48dcd7a7e9cf1b42dfb2551055914ebb
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-24 01:06:23 +0200

    feat(storybook): add loader stories

--- client/design-system/components/Loader/Loader.tsx
+++ client/design-system/components/Loader/Loader.tsx
@@ -3,11 +3,12 @@ import cx from 'classnames';
 
 import styles from './Loader.module.scss';
 
-type LoaderProps = {
+export type LoaderProps = {
   className?: string;
   size?: number;
   thickness?: number;
 };
+
 const Loader = ({ className, size = 30, thickness = 3 }: LoaderProps) => (
   <div
     className={cx(className, styles.loader)}
