f57d0e815186b86ec6ca17b6995c454af367844e
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-05-26 12:28:39 +0200

    feat(builds:gear): add items grid

--- client/features/builds/components/EditorSections/Gear/Gear.tsx
+++ client/features/builds/components/EditorSections/Gear/Gear.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
 
+import { AddIcon } from '#design-system/icons';
+
 import EditorSectionWrapper from '../EditorSectionWrapper';
+
+import styles from './Gear.module.scss';
+
 const Gear = () => {
-  return <EditorSectionWrapper section="Gear">Gear</EditorSectionWrapper>;
+  return (
+    <EditorSectionWrapper section="Gear">
+      <div className={styles.grid}>
+        <div style={{ gridArea: 'main-weapon' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'off-hand' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'helmet' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'ring-1' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'ring-2' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'amulet' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'gloves' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'body' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'boots' }}>
+          <AddIcon />
+        </div>
+        <div style={{ gridArea: 'belt' }}>
+          <AddIcon />
+        </div>
+      </div>
+    </EditorSectionWrapper>
+  );
 };
 
 export default Gear;
