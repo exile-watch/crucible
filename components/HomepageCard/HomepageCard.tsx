51673203d3bdcff9ff69c8e055b349c9bdfd0451
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-02-29 07:13:19 +0100

    perf: debounce video(s) playing after mouseovers

--- components/HomepageCard/HomepageCard.tsx
+++ components/HomepageCard/HomepageCard.tsx
@@ -2,21 +2,34 @@ import {Card, SimpleGrid, Text} from "@mantine/core";
 import styles from './HomepageCard.module.scss'
 import Link from "next/link";
 import HomepageVideo from "./HomepageVideo/HomepageVideo";
-import {useState} from "react";
-
+import {useState, useCallback, useEffect} from "react";
+import {debounce} from 'lodash'
 interface HomepageCardProps {
 
 }
 
 const HomepageCard = ({name, gif, path, isCategory = true}) => {
   const [isHovering, setIsHovering]=useState(false);
+  const DELAY = 1000;
 
-  const handleMouseOver = () => {
+  // Without debouncing the mouseover, we can start requests immediately
+  // In an "edge" case where a "curious" user decides to furiously...
+  // ...mouseover and mouseleave over 20+ video elements (homepage)...
+  // ...this can lead to huge memory overload causing page crashes
+  const handleMouseOver = useCallback(debounce(() => {
     setIsHovering(true);
-  }
+  }, DELAY), []);
+
   const handleMouseLeave = () => {
     setIsHovering(false);
+    handleMouseOver.cancel();
   }
+
+  useEffect(() => {
+    return () => {
+      handleMouseOver.cancel();
+    };
+  }, [handleMouseOver]);
 
   return (
     <Card className={styles.card} shadow="md" component={Link} href={path ?? "#"} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
