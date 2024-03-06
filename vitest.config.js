910ea9feac1276efabfc3245771f5550de1c8e23
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-03-06 18:36:32 +0100

    refactor: convert to rollup package

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,4 @@
+import { defaultConfig, mergeConfig } from "@exile-watch/unit-testing-config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default {
-  plugins: [tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./test-setup.tsx"],
-  },
-};
+export default mergeConfig(defaultConfig);
