3f3fe929f2e4f369c108e55d91f6509d4006d13f
Sebastian Krzyżanowski <eskrzy@gmail.com>
2024-04-14 13:32:29 +0200

    refactor: move directory data fetching logic to ssg getStaticProps

--- pages/[directory]/index.ts
+++ pages/[directory]/index.ts
@@ -1,2 +1,37 @@
 import { DirectoryPage } from "#features/pages";
+import {checkIfDirExists, directoryPaths} from "#features/directory/Directory.api";
+import {GetStaticPaths, GetStaticProps} from "next";
+
+type GetStaticPropsType = {
+  directory: string;
+}
+
+export const getStaticPaths = (async () => {
+  return {
+    paths: directoryPaths,
+    fallback: 'blocking'
+  };
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async (context) => {
+  const { params } = context;
+
+  if (!params) {
+    return { notFound: true };
+  }
+
+  const { directory } = params;
+
+  const dirExists = await checkIfDirExists(directory);
+
+  if (!dirExists) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {},
+    revalidate: 60, // Revalidate at most once per minute
+  };
+}) satisfies GetStaticProps<{}, GetStaticPropsType>
+
 export default DirectoryPage;
